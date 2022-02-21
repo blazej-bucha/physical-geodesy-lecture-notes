8bccad283d2dbcdc54ee5c629a10bf83c67eca2c
Blazej Bucha <blazej.bucha@stuba.sk>
2022-02-21 15:02:23 +0100

    add Legendre functions

---
+++ figs/legendre-functions/legendre-functions.py
@@ -0,0 +1,36 @@
+# Import modulov
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+# Maximálny stupeň Legendreových polynómov
+nmax = 5
+
+# Vzorkovanie sférických šírok a ich sínusov
+lat = np.linspace(-np.pi / 2.0, np.pi / 2.0, 101)
+t   = np.sin(lat)
+
+# Explicitný výpočet Legendreových funkcií
+p00 = np.ones(t.shape)
+p10 = t
+p11 = np.sqrt(1.0 - t**2)
+p20 = 0.5 * (3.0 * t**2 - 1)
+p21 = 3.0 * t * np.sqrt(1.0 - t**2)
+p22 = 3.0 * (1.0 - t**2)
+
+# Vykreslenie
+fig, ax = plt.subplots(figsize=(14.0 / 2.54, 9.0 / 2.54))
+ax.plot(t, p00, label="$P_{0,0}$", color='#1f77b4')
+ax.plot(t, p10, label="$P_{1,0}$", color='#ff7f0e')
+ax.plot(t, p11, label="$P_{1,1}$", color='#ff7f0e', linestyle="dashed")
+ax.plot(t, p20, label="$P_{2,0}$", color='#2ca02c')
+ax.plot(t, p21, label="$P_{2,1}$", color='#2ca02c', linestyle="dashed")
+ax.plot(t, p22, label="$P_{2,2}$", color='#2ca02c', linestyle="dotted")
+ax.grid(visible=True)
+ax.set_xlabel("$t$")
+ax.legend(loc="center", bbox_to_anchor=(0.5, -0.35), ncol=nmax + 1)
+fig.subplots_adjust(bottom=0.3, top=0.98)
+rc('text', usetex=True)
+plt.show()
+
+fig.savefig("../../figs/legendre-functions.pdf")
