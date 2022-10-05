1867d1bf22512372f7f39aa3e3cf8d6439600be6
Blazej Bucha <blazej.bucha@stuba.sk>
2022-10-05 15:16:32 +0200

    add hotine and poisson integrals

---
+++ figs/hotine-kernel/hotine-kernel.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('text', usetex=True)
+
+# Computation of the Hotine function
+psi    = np.linspace(5.0 * np.pi / 180.0, np.pi, 100)
+hotine = 1.0 / np.sin(psi / 2.0) - np.log(1.0 + 1.0 / np.sin(psi / 2.0))
+
+# Plotting
+fig, ax = plt.subplots(figsize=(14.0 / 2.54, 8.0 / 2.54))
+ax.plot(psi * 180.0 / np.pi, hotine)
+ax.set_xlabel("$\psi$ $(^\circ)$")
+ax.set_ylabel("$H(\psi)$")
+ax.grid(visible=True)
+plt.show()
+
+fig.savefig("../../latex/fig-hotine-kernel.pdf")
